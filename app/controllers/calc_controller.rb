fb9701cfcada9b2a85f788a32c80034d543834b9
Lukasz Kapica <kapicaluk@Lukaszs-MacBook-Pro.local>
2016-03-14 13:42:09 -0400

    Homework 03.14

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -5,12 +5,120 @@ class CalcController < ApplicationController
 	def add
 	end
 
+	def doadd
+		ok = true
+		if calc_params[:firstNum].blank?
+			flash.now[:notice1] = "the firs number is required. "
+			ok = false
+		end	
+
+		if calc_params[:secondNum].blank?
+			flash.now[:notice2] = "the second number is required."
+			ok = false
+		end
+
+if ok 
+
+		@firstNum = calc_params[:firstNum].to_i
+		@secondNum = calc_params[:secondNum].to_i
+		@result = @firstNum + @secondNum
+else
+	render "add"
+end
+	end
+
 	def substract
 	end
 
+	def dosubstract 
+		ok = true
+		if calc_params[:firstNum].blank?
+			flash.now[:notice1] = "the first number is required. "
+			ok = false
+		end	
+
+		if calc_params[:secondNum].blank?
+			flash.now[:notice2] = "the second number is required."
+			ok = false
+		end
+if ok 
+
+		@firstNum = calc_params[:firstNum].to_i
+		@secondNum = calc_params[:secondNum].to_i
+		@result = @firstNum - @secondNum
+else
+	render "substract"
+
+end
+end
+
+
+
 	def multiply
 	end
-	
+
+
+	def domultiply
+		ok = true
+		if calc_params[:firstNum].blank?
+			flash.now[:notice1] = "the first number is required. "
+			ok = false
+		end	
+
+		if calc_params[:secondNum].blank?
+			flash.now[:notice2] = "the second number is required."
+			ok = false
+		end
+if ok 
+
+		@firstNum = calc_params[:firstNum].to_i
+		@secondNum = calc_params[:secondNum].to_i
+		@result = @firstNum * @secondNum
+else
+	render "multiply"
+
+end
+end
+
+
 	def divide
 	end
+
+	def dodivide
+		ok = true
+		if calc_params[:firstNum].blank?
+			flash.now[:notice1] = "the first number is required. "
+			ok = false
+		end	
+
+		if calc_params[:secondNum].blank?
+			flash.now[:notice2] = "the second number is required."
+			ok = false
+		end
+
+
+
+
+if ok 
+
+		@firstNum = calc_params[:firstNum].to_i
+		@secondNum = calc_params[:secondNum].to_i
+		@result = @firstNum / @secondNum
+else
+	render "divide"
+
 end
+end
+
+
+
+
+end
+
+private
+def calc_params
+	params.permit(:firstNum, :secondNum)
+
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get 'calc/substract' => 'calc#substract'
   get 'calc/multiply' => 'calc#multiply'
   get 'calc/divide' => 'calc#divide'
+  post 'calc_doadd' => 'calc#doadd'
+  post 'calc_dosubstract' => 'calc#dosubstract'
+  post 'calc_domultiply' => 'calc#domultiply'
+  post 'calc_dodivide' => 'calc#dodivide'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
